7684ef398183445b240b8360042e81d842d3a7ec
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2024-04-18 14:10:19 -0300

    feat: style search page

---
+++ src/pages/search/script.js
@@ -0,0 +1,35 @@
+const searchPage = {
+  createFilterButton: function () {
+    // create content filter
+    const contentFilter = document.createElement('div');
+    contentFilter.classList.add('content-button-filter');
+    
+    // create button filter
+    const buttonFilter = document.createElement('div');
+    buttonFilter.classList.add('filter-button');
+    buttonFilter.innerHTML = `
+      <span>Filtrar por</span>
+    `
+    const contentButton = document.createElement('div');
+    contentButton.classList.add('content-close')
+
+    const buttonClose = document.createElement('div');
+    buttonClose.classList.add('close');
+    buttonClose.innerHTML = `X`;
+    contentButton.appendChild(buttonClose);
+    
+    contentFilter.appendChild(buttonFilter);
+    contentFilter.addEventListener('click', ()=>{
+      document.querySelector('.coluna').classList.toggle('show-filter')
+    })
+    document.querySelector('.ordenar-listagem > div').prepend(contentFilter);
+    document.querySelector('.coluna').prepend(contentButton);
+  },
+  
+  init: function () {
+    console.log('init filter');
+    this.createFilterButton();
+  }
+}
+
+window.location.pathname == '/buscar' ? searchPage.init() : null;
