1cbf6c4e37475cc981c8e3e1a4d10a4fb1845469
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2024-04-21 14:04:36 -0300

    feat: style main banner / barra newsletter

---
+++ src/components/Newsletter/scripts.js
@@ -0,0 +1,27 @@
+const barraNewsLetter = {
+  createElements: function () {
+    const hands = document.createElement("div");
+    hands.classList.add("hands-newsletter");
+
+    const flowers = document.createElement("div");
+    flowers.classList.add("flowers-newsletter");
+
+    document.querySelector("#barraNewsletter .newsletter").appendChild(hands);
+    document.querySelector("#barraNewsletter .newsletter").appendChild(flowers);
+  },
+  moveDescription: () => {
+    document
+      .querySelector(".newsletter .interno .titulo")
+      .appendChild(
+        document.querySelector(".newsletter .interno .newsletter-cadastro")
+      );
+  },
+  init: function () {
+    this.createElements();
+    this.moveDescription();
+  },
+};
+
+document.querySelector("#barraNewsletter .newsletter")
+  ? barraNewsLetter.init()
+  : null;
